0b5f69c62ca7730d38c3ecfb83e6a4e8522639cc
Kerem Gündüz <keremgunduz@MacBook-Pro.local>
2022-05-14 19:11:09 +0300

    old HTML files are deleted, new ones are added.

--- practice-app/comment_info/urls.py
+++ practice-app/comment_info/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('api/<int:id>',commentList.as_view()),
+    path('api/<int:id>',commentList.as_view(), name="base"),
     path('', index, name="index"),
     path('SearchResult', requestGetter, name="requestGetter"),
     path('UpdatedResult', requestPoster, name="requestPoster"),
