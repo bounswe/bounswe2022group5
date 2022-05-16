9a94afadc89b60fafac53e97d10a3e7bd52e1c38
palahb <palahb@gmail.com>
2022-05-16 18:17:16 +0300

    Initial Frontend is designed for comment endpoint. Some minor fixes are performed.

--- practice-app/comment/serializers.py
+++ practice-app/comment/serializers.py
@@ -3,4 +3,5 @@ from .models import Comment
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
-        fields = ["body", "user", "timestamp", "post", "city_name", "weather"]
+        fields = ["body", "user", "timestamp", "post", 
+            "city_name", "weather", "nof_upvotes", "nof_downvotes"]
--- practice-app/comment/urls.py
+++ practice-app/comment/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path, include
 from .views import (
+    index,
+    getComments,
+    postComment,
     CommentApiView
 )
 
 urlpatterns = [
+    path('', index, name="index"),
+    path('list/', getComments, name="getComments"),
+    path('create/', postComment, name="postComment"),
     path('api/<int:post_id>/', CommentApiView.as_view()),
 ]
