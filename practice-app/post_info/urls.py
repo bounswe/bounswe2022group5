c5789c38dcccab165b4d217b0f1f658ec44b80e9
kardelendemiral <74200767+kardelendemiral@users.noreply.github.com>
2022-05-16 14:36:41 +0300

    Testing and some small fixes
    Unit tests for post-info are implemented.
    Unnecessary serializer is removed.

--- practice-app/post_info/urls.py
+++ practice-app/post_info/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('get/', views.getPost, name="getPost"),
     path('update/', views.postPost, name="postPost"),
-    path('api/<int:pk>/', views.PostInfo.as_view()),
+    path('api/<int:pk>/', views.PostInfo.as_view(), name='api'),
 ]
