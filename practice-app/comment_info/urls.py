bd7f4df79631ffe022a605a1142c61ce606f4cbe
Kerem Gündüz <keremgunduz@MacBook-Pro.local>
2022-05-18 23:10:32 +0300

    Some url errors solved. Also there was a baseURL that makes some conflicts while dockering the project. It is removed and replaced it's functionality with a good manner

--- practice-app/comment_info/urls.py
+++ practice-app/comment_info/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('api/<int:id>',commentList.as_view(), name="base"),
+    path('api/<int:pk>',commentList.as_view(), name="base"),
     path('', commentInfoIndex, name="comment-info-index"),
     path('SearchResult', requestGetter, name="requestGetter"),
     path('UpdatedResult', requestPoster, name="requestPoster"),
