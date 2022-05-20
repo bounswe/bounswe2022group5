aa20982cc961d476848f43d1164ac962a31d45b8
kardelendemiral <74200767+kardelendemiral@users.noreply.github.com>
2022-05-20 11:14:09 +0300

    Bug-fix for tests

--- practice-app/post_info/urls.py
+++ practice-app/post_info/urls.py
@@ -2,8 +2,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='index_post_info'),
     path('get/', views.getPost, name="getPost"),
     path('update/', views.postPost, name="postPost"),
-    path('api/<int:pk>/', views.PostInfo.as_view(), name='api'),
+    path('api/<int:pk>/', views.PostInfo.as_view(), name='api_post_info'),
 ]
