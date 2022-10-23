fb6b4a32d4a8b3975a60e645a475869974f6166a
irfan <irfan@snickerdoodlelabs.io>
2022-10-23 22:19:34 +0300

    Unit tests for model classes

--- app/backend/backend/models.py
+++ app/backend/backend/models.py
@@ -4,11 +4,11 @@
 
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
-
+import datetime
 
 class User(models.Model):
     password = models.CharField(max_length=200, null=False)
-    email = models.CharField(max_length=100, null=False)
+    email = models.CharField(max_length=100, null=False, unique=True)
 
     type = models.IntegerField(null=False)
 
@@ -18,7 +18,7 @@ class User(models.Model):
 
 class Admin(User):
     # user = models.ForeignKey(User, on_delete=models.CASCADE)
-    username = models.CharField(max_length=50, null=False)
+    username = models.CharField(max_length=50, null=False, unique=True)
 
     def __str__(self):
         return self.username
@@ -28,7 +28,7 @@ class MemberInfo(models.Model):
     lastname = models.CharField(max_length=25, null=False)
     address = models.CharField(max_length=100, null=True)
     weight = models.DecimalField(null=True, max_digits=3, decimal_places=1)
-    height = models.DecimalField(null=True, max_digits=3, decimal_places=1)
+    height = models.IntegerField(null=True)
     age = models.IntegerField(null=True)
 
     past_illnesses = ArrayField(
@@ -49,9 +49,8 @@ class MemberInfo(models.Model):
 
 
 class Member(User):
-    username = models.CharField(max_length=50, null=False)
-    avatar = models.CharField(max_length=200, null=False)
-    banned_by = models.CharField(max_length=50, null=True)  # username of admin
+    username = models.CharField(max_length=50, null=False, unique=True)
+    banned_by = models.CharField(max_length=50, null=True, default=None)  # username of admin
 
     info = models.ForeignKey(MemberInfo, null=False, on_delete=models.CASCADE)
 
@@ -62,14 +61,14 @@ class Member(User):
         if self.info.lastname != None:
             retval += self.info.firstname
         
-        if retval != " ":
+        if retval != "":
             return retval
 
         return self.username
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=50, null=False)
+    name = models.CharField(max_length=50, null=False, unique=True)
     definition = models.CharField(max_length=100, null=True)
 
 
@@ -87,6 +86,10 @@ class Report(models.Model):
     reporter_user = models.ForeignKey(User, null=False, on_delete=models.CASCADE, related_name='reporter_user')
     reported_user = models.ForeignKey(User, null=False, on_delete=models.CASCADE, related_name='reported_user')
     message = models.CharField(max_length=500, null=False)
+    date = models.DateField(null=False, default=datetime.date.today)
     reviewed_by = models.ForeignKey(Admin, null=True, default=None, on_delete=models.SET_NULL, related_name='reviewed_by')
+    reviewed_date = models.DateField(null=True)
 
 
+    def __str__(self):
+        return self.message
---
+++ app/backend/tests/constants.py
@@ -0,0 +1,33 @@
+TEST_PASSWORD = "test_password"
+
+TEST_ADMIN_EMAIL = "test_admin@test.com"
+TEST_ADMIN_USERNAME = "test_admin_username"
+
+TEST_CATEGORY_NAME = "Test Category"
+TEST_CATEGORY_DEFINITION = "Definition for Test Category"
+
+TEST_DOCTOR_EMAIL = "test_doctor@test.com"
+TEST_DOCTOR_FULLNAME = "Testname Testsurname"
+TEST_DOCTOR_HOSPITAL_NAME = "Rumeli Tıp"
+
+TEST_MEMBER_EMAIL = "test_member@test.com"
+TEST_MEMBER_USERNAME = "test_member_username"
+TEST_MEMBER_FIRSTNAME = "test_member_firstname"
+TEST_MEMBER_LASTNAME = "test_member_lastname"
+TEST_MEMBER_ADDRESS = "Istanbul"
+TEST_MEMBER_WEIGHT = 75.5
+TEST_MEMBER_HEIGHT = 183
+TEST_MEMBER_AGE = 25
+TEST_MEMBER_PAST_ILLNESSES = ["cancer", "vertigo"]
+TEST_MEMBER_ALLERGIES = ["pollen", "peanut"]
+TEST_MEMBER_CHRONIC_DISEASES = ["down syndrome"]
+TEST_MEMBER_UNDERGONE_OPERATIONS = None
+TEST_MEMBER_USED_DRUGS = ["LSD", "DMT", "Weed"]
+
+TEST_MEMBER_2_EMAIL = "test_member_2@test.com"
+TEST_MEMBER_2_USERNAME = "test_member_2_username"
+
+TEST_MEMBER_3_EMAIL = "test_member_3@test.com"
+TEST_MEMBER_3_USERNAME = "test_member_3_username"
+
+TEST_REPORT_MESSAGE = "This user called me an idiot."
