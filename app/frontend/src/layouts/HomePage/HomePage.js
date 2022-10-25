4361a9b075f4393b15e0ec03bb518f133545029c
Burak Mert <burakmert236@gmail.com>
2022-10-25 19:51:38 +0300

    import user state in homepage layout

--- app/frontend/src/layouts/HomePage/HomePage.js
+++ app/frontend/src/layouts/HomePage/HomePage.js
@@ -1,6 +1,9 @@
 import React from "react";
+import { useSelector } from 'react-redux';
 
 const HomePageLayout = () => {
+    const { status: userStatus } = useSelector((state) => state.user);
+
     return(
         <div>
             HOMEPAGE
