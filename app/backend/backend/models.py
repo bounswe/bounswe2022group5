e28fba81760d80d19e69f5867da5a8d236889319
Mehmet Emre Akbulut <emre@themindsite.com>
2022-10-26 21:36:00 +0300

    auth tables revised

--- app/backend/backend/models.py
+++ app/backend/backend/models.py
@@ -5,12 +5,39 @@
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
 import datetime
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, AbstractUser, UserManager
 from django.contrib.auth.base_user import BaseUserManager
 
+class CustomUserManager(UserManager):
+    def _create_user(self, email, password, **extra_fields):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, type=0, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
-class CustomUser(AbstractBaseUser):
-    
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+class CustomUser(AbstractUser):
+    objects = CustomUserManager()
+    username = None
+    first_name = None
+    groups = None
+    last_name = None
     password = models.CharField(max_length=200, null=False)
     email = models.CharField(max_length=100, null=False, unique=True) 
 
