39ce64d6a5d1606544b3a9929a8da3304737c9cb
Oozankilic <ozan.kilic@boun.edu.tr>
2022-10-27 16:29:59 +0300

    Forum page is implemented
    Forum page is created but backend connection is not done

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePageLayout from './layouts/HomePage/HomePage';
 import Login from './pages/Login/Login';
 import SignUp from './pages/SignUp/SignUp';
+import Forum from './layouts/Forum/Forum';
 
 import './App.css';
 import 'antd/dist/antd.css'
@@ -16,6 +17,8 @@ function App() {
         <Route path='/signup' element={ <SignUp /> }/>
 
         <Route path='/login' element={ <Login /> }/>
+
+        <Route path='/forum' element={ <Forum /> } />
       </Routes>
     </Router>
   );
