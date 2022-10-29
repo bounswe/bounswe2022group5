bfb8ab77832f907bb9cdce36dc1482d50ad71fb4
Kerem Gündüz <keremgunduz@MacBook-Pro.home>
2022-10-29 19:13:08 +0300

    Navbar file location changed

--- app/frontend/src/layouts/HomePage/HomePage.js
+++ app/frontend/src/layouts/HomePage/HomePage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useSelector } from 'react-redux';
 
-import NavBar from "./NavBar";
+import NavBar from "../NavBar/NavBar";
 
 const HomePageLayout = () => {
     const { status: userStatus } = useSelector((state) => state.user);
