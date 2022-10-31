666cd93c41b1c63022a8270af446b144ed7d3e9c
Baver Bengin Bestas <b.benginbestas@hotmail.com>
2022-10-31 14:15:54 +0300

    Added error messages when giving unwanted input

--- app/mobile/bounswe5_mobile/lib/screens/login.dart
+++ app/mobile/bounswe5_mobile/lib/screens/login.dart
@@ -5,6 +5,16 @@ import 'package:bounswe5_mobile/screens/signup.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+import '../API_service.dart';
+
+bool isEmail(String em) {
+  //validator for email
+  String p =
+      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+  RegExp regExp = new RegExp(p);
+  return regExp.hasMatch(em);
+}
+
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
@@ -13,6 +23,21 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _email = TextEditingController();
+  final TextEditingController _pass = TextEditingController();
+  bool isMember = true;
+
+  Future<bool> login(String email, String password) async {
+    // Login API call handling function
+    final result = await ApiService().login(email, password);
+    if (result == 200) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,117 +52,158 @@ class _LoginPageState extends State<LoginPage> {
         ),
         elevation: 0.0,
       ),
-      body: Center(
-        child: SingleChildScrollView(
-          // To prevent an exception when using keyboard on phone
-          child: Column(children: [
-            // Text for Login
-            Text(
-              'Login',
-              style: TextStyle(
-                  fontStyle: FontStyle.normal,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 60,
-                  color: Colors.blue[900]),
-            ),
-            Text(
-              'for user benefits!',
-              style: TextStyle(
-                fontSize: 40,
-                color: Colors.blue[900],
+      body: Form(
+        key: _formKey,
+        child: Center(
+          child: SingleChildScrollView(
+            // To prevent an exception when using keyboard on phone
+            child: Column(children: [
+              // Text for Login
+              Text(
+                'Login',
+                style: TextStyle(
+                    fontStyle: FontStyle.normal,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 60,
+                    color: Colors.blue[900]),
               ),
-            ),
-            SizedBox(height: 10),
+              Text(
+                'for user benefits!',
+                style: TextStyle(
+                  fontSize: 40,
+                  color: Colors.blue[900],
+                ),
+              ),
+              SizedBox(height: 10),
 
-            // E-mail field
-            Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: TextField(
+              // E-mail field
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: TextFormField(
+                  controller: _email,
                   decoration: InputDecoration(
-                hintText: 'E-mail',
-                prefixIcon: Icon(Icons.email_rounded),
-              )),
-            ),
+                    hintText: 'E-mail',
+                    prefixIcon: Icon(Icons.email_rounded),
+                  ),
+                  // Error if user gives unwanted input
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your email address';
+                    } else if (!isEmail(value)) {
+                      return 'Please enter a valid email address';
+                    }
+                    return null;
+                  },
+                ),
+              ),
 
-            // Password field
-            Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: TextField(
+              // Password field
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: TextFormField(
                   obscureText: true,
                   decoration: InputDecoration(
                     hintText: 'Password',
                     prefixIcon: Icon(Icons.lock),
-                  )),
-            ),
-
-            // Login button below user information
-            Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Container(
-                padding: EdgeInsets.all(20.0),
-                decoration: BoxDecoration(
-                  color: Colors.blue[600],
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: Center(
-                    child: Text(
-                  'Login',
-                  style: TextStyle(
-                    fontStyle: FontStyle.normal,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20.0,
                   ),
-                )),
-              ),
-            ),
-
-            // User can sign up if they don't have account
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Don\'t have an account? ',
-                ),
-                InkWell(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const SignupPage()),
-                    );
+                  validator: (value) {
+                    // Error if user gives unwanted input
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your password';
+                    }
+                    return null;
                   },
-                  child: Text(
-                    'Sign up',
-                    style: TextStyle(
-                      color: Colors.orange[900],
-                      fontWeight: FontWeight.bold,
+                ),
+              ),
+
+              // Login button below user information
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: ElevatedButton(
+                  onPressed: () async {
+                    if (_formKey.currentState!.validate()) {
+                      int type;
+                      bool registered = await login(_email.text, _pass.text);
+                      if (registered) {
+                        //if registered successfully, go to the login page
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const LoginPage()),
+                        );
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('Could not register')),
+                        );
+                      }
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue[600]),
+                  child: Container(
+                    padding: EdgeInsets.all(20.0),
+                    child: Center(
+                        child: Text(
+                      'Login',
+                      style: TextStyle(
+                        fontStyle: FontStyle.normal,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20.0,
+                        color: Colors.black,
+                      ),
+                    )),
+                  ),
+                ),
+              ),
+
+              // User can sign up if they don't have account
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Don\'t have an account? ',
+                  ),
+                  InkWell(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const SignupPage()),
+                      );
+                    },
+                    child: Text(
+                      'Sign up',
+                      style: TextStyle(
+                        color: Colors.orange[900],
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
-                ),
-                Text('!'),
-              ],
-            ),
+                  Text('!'),
+                ],
+              ),
 
-            // A sized box for seperating texts
-            SizedBox(height: 50),
+              // A sized box for seperating texts
+              SizedBox(height: 50),
 
-            // continue without login button
-            InkWell(
-              onTap: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(builder: (context) => const HomePage()),
-                );
-              },
-              child: Text(
-                'Continue without logging in',
-                style: TextStyle(
-                  color: Colors.black38,
-                  fontStyle: FontStyle.italic,
+              // continue without login button
+              InkWell(
+                onTap: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => const HomePage()),
+                  );
+                },
+                child: Text(
+                  'Continue without logging in',
+                  style: TextStyle(
+                    color: Colors.black38,
+                    fontStyle: FontStyle.italic,
+                  ),
                 ),
               ),
-            ),
-          ]),
+            ]),
+          ),
         ),
       ),
     );
