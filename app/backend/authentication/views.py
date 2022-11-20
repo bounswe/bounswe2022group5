bce23c945b0281176bfc818fd3e9ccfcbc244fc0
Mehmet Emre Akbulut <emre@themindsite.com>
2022-11-20 13:47:48 +0300

    tests were revised w.r.t. changes on registration

--- app/backend/authentication/views.py
+++ app/backend/authentication/views.py
@@ -1,14 +1,13 @@
 from backend.models import CustomUser, Doctor, Member, Category, MemberInfo
 from .serializers import UserSerializer, RegistrationSerializer, DoctorSerializer, MemberSerializer
 from django.contrib.auth import get_user_model
-from django.core.exceptions import ValidationError
-from django.db import IntegrityError
 from django.contrib.auth import login, logout
 from django.contrib.auth.hashers import check_password
 from rest_framework import viewsets
 
 # Create your views here.
 from rest_framework.views import APIView
+from rest_framework.exceptions import ValidationError, ParseError
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
@@ -86,12 +85,10 @@ def register_user(request):
             return Response(status=400,data=data)
 
         return Response(status=200,data=data)
-    except IntegrityError as e:
+    except ValidationError as e:
         
         raise ValidationError({"400": f'{str(e)}'})
 
-    except KeyError as e:
-        raise ValidationError({"400": f'Field {str(e)} missing'})
 
 @api_view(["POST"])
 @permission_classes([AllowAny])
@@ -101,10 +98,12 @@ def login_user(request):
         reqBody = json.loads(request.body)
         email1 = reqBody['email']
         password = reqBody['password']
-        try:
+        
+        if User.objects.filter(email = email1).exists():
             Account = User.objects.get(email=email1)
-        except BaseException as e:
-            raise ValidationError({"400": f'{str(e)}'})
+        else:
+            raise ValidationError({"message":"Incorrect Login credentials"})
+        
 
         token = Token.objects.get_or_create(user=Account)[0].key
         if not check_password(password, Account.password):
