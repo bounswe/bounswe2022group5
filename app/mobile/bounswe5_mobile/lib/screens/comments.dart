34204f9a42acf8a7a80c0f23fb51342c610228e7
enginoguzhansenol <engin.senol@boun.edu.tr>
2022-11-28 15:55:02 +0300

    Initial pages for activity history is created.

---
+++ app/mobile/bounswe5_mobile/lib/screens/comments.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:bounswe5_mobile/models/user.dart';
+import 'package:bounswe5_mobile/API_service.dart';
+import '../widgets/ProfileListItem.dart';
+import '../screens/profile.dart';
+import '../widgets/ProfileWidget.dart';
+
+class CommentsPage extends StatefulWidget {
+  const CommentsPage({Key? key}) : super(key: key);
+
+  @override
+  State<CommentsPage> createState() => _CommentsPageState();
+}
+
+class _CommentsPageState extends State<CommentsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: const [Text(
+              'Logo',
+              style: TextStyle(
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              )
+          )
+          ],
+        ),
+        elevation: 0.0,
+      ),
+    );
+  }
+}
---
+++ app/mobile/bounswe5_mobile/lib/screens/upvotes.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:bounswe5_mobile/models/user.dart';
+import 'package:bounswe5_mobile/API_service.dart';
+import '../widgets/ProfileListItem.dart';
+import '../screens/profile.dart';
+import '../widgets/ProfileWidget.dart';
+
+class UpvotesPage extends StatefulWidget {
+  const UpvotesPage({Key? key}) : super(key: key);
+
+  @override
+  State<UpvotesPage> createState() => _UpvotesPageState();
+}
+
+class _UpvotesPageState extends State<UpvotesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: const [Text(
+              'Logo',
+              style: TextStyle(
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              )
+          )
+          ],
+        ),
+        elevation: 0.0,
+      ),
+    );
+  }
+}
