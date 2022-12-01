5c3a00e1f144d4d3649a10ca226ecf09e5db36e3
Alper Canberk Balcı <a.canberk.balci@gmail.com>
2022-12-02 00:39:06 +0300

    user_profile -> get_personal_info and update_personal_info implemented. No unit tests.

--- app/backend/user_profile/views.py
+++ app/backend/user_profile/views.py
@@ -8,6 +8,8 @@ from common.views import upload_to_s3, delete_from_s3
 
 from backend.constants import UserType
 from backend.models import CustomUser
+from backend.models import MemberInfo
+from backend.models import Member
 
 from backend.constants import UserType
 from forum.models import Post
@@ -116,3 +118,258 @@ def get_upvoted_posts(request):
     result_page = paginator.paginate_queryset(posts, request)
     serializer = PostSerializer(result_page, many=True)
     return paginator.get_paginated_response(serializer.data)
+
+
+
+@api_view(['GET',])
+@permission_classes([IsAuthenticated,])
+def get_personal_info(request):
+
+    user = request.user
+    personal_info = {}
+
+    try:
+        self_info = CustomUser.objects.get(user=user)
+    except:
+        return Response({'error': 'User not found'}, status=400)
+    
+    email = self_info.email
+    personal_info['email'] = email
+        
+    date_of_birth = self_info.date_of_birth
+    personal_info['date_of_birth'] = date_of_birth
+        
+    register_date = request.user.date_joined
+    personal_info['register_date'] = register_date
+
+    # Initialization of fields: (to return null values as "-1". Frontend will handle it.)
+    personal_info['type'] =  "-1"
+    personal_info['full_name'] = "-1"
+    personal_info['specialization'] = "-1"
+    personal_info['hospital_name'] = "-1"
+    personal_info['verified'] = "-1"
+    personal_info['document'] = "-1"
+    personal_info['profile_picture'] = "-1"
+    personal_info['member_username'] = "-1"
+    personal_info['firstname'] = "-1"
+    personal_info['lastname'] = "-1"
+    personal_info['address'] = "-1"
+    personal_info['weight'] = "-1"
+    personal_info['height'] = "-1"
+    personal_info['age'] = "-1"
+    personal_info['avatar'] = "-1"
+    personal_info['past_illnesses'] = "-1"
+    personal_info['allergies'] = "-1"
+    personal_info['chronic_diseases'] = "-1"
+    personal_info['undergone_operations'] = "-1"
+    personal_info['used_drugs'] =  "-1"
+
+
+    if int(user.type) == UserType.DOCTOR.value:
+
+        doctor = Doctor.objects.get(user=user) # or request.user ?
+        personal_info['type'] =  1
+        full_name = doctor.full_name
+        specialization = doctor.specialization
+        hospital_name = doctor.hospital_name
+        verified = doctor.verified
+        document = doctor.document
+        profile_picture = doctor.profile_picture
+
+        personal_info['full_name'] = full_name
+        personal_info['specialization'] = specialization
+        personal_info['hospital_name'] = hospital_name
+        personal_info['verified'] = verified
+        personal_info['document'] = document
+        personal_info['profile_picture'] = profile_picture
+        
+
+    elif int(user.type) == UserType.MEMBER.value:
+
+        member = Member.objects.get(user=user) # or request.user ? or id=request.user.id
+        personal_info['type'] =  2
+        member_info = member.info
+        # member_info_alternative = MemberInfo.objects.get(user=user) there is no primary key, so I used foreign key above.
+
+        member_username = member.member_username
+        firstname = member_info.firstname
+        lastname = member_info.lastname
+        address = member_info.address
+        weight = member_info.weight
+        height = member_info.height
+        age = member_info.age
+        avatar = member_info.avatar
+        past_illnesses = member_info.past_illnesses
+        allergies = member_info.allergies
+        chronic_diseases = member_info.chronic_diseases
+        undergone_operations = member_info.undergone_operations
+        used_drugs = member_info.used_drugs
+
+        personal_info['member_username'] = member_username
+        personal_info['firstname'] = firstname
+        personal_info['lastname'] = lastname
+        personal_info['address'] = address
+        personal_info['weight'] = weight
+        personal_info['height'] = height
+        personal_info['age'] = age
+        personal_info['avatar'] = avatar
+        personal_info['past_illnesses'] = past_illnesses
+        personal_info['allergies'] = allergies
+        personal_info['chronic_diseases'] = chronic_diseases
+        personal_info['undergone_operations'] = undergone_operations
+        personal_info['used_drugs'] = used_drugs
+    
+    return Response(personal_info, status=200)
+    
+
+@api_view(['POST',])
+@permission_classes([IsAuthenticated,])
+def update_personal_info(request):
+    
+    user = request.user
+    data = request.data
+    try:
+        self_info = CustomUser.objects.get(user=user)
+    except:
+        return Response({'error': 'User not found'}, status=400)
+
+    personal_info = {}
+
+    email = data['email']
+    self_info.email = email
+
+    date_of_birth = data['date_of_birth']
+    self_info.date_of_birth = date_of_birth
+
+    personal_info['email'] = email
+    personal_info['date_of_birth'] = date_of_birth
+    
+    self_info.save()
+    
+    # Initialization of fields: (to return null values as "-1". Frontend will handle it.)
+    personal_info['type'] =  "-1"
+    personal_info['full_name'] = "-1"
+    personal_info['specialization'] = "-1"
+    personal_info['hospital_name'] = "-1"
+    personal_info['verified'] = "-1"
+    personal_info['document'] = "-1"
+    personal_info['profile_picture'] = "-1"
+    personal_info['member_username'] = "-1"
+    personal_info['firstname'] = "-1"
+    personal_info['lastname'] = "-1"
+    personal_info['address'] = "-1"
+    personal_info['weight'] = "-1"
+    personal_info['height'] = "-1"
+    personal_info['age'] = "-1"
+    personal_info['avatar'] = "-1"
+    personal_info['past_illnesses'] = "-1"
+    personal_info['allergies'] = "-1"
+    personal_info['chronic_diseases'] = "-1"
+    personal_info['undergone_operations'] = "-1"
+    personal_info['used_drugs'] =  "-1"
+
+    if int(user.type) == UserType.DOCTOR.value:
+
+        try:
+            doctor = Doctor.objects.get(user=user) # or request.user ?
+        except:
+            return Response({'error': 'User not found'}, status=400)
+
+        personal_info['type'] =  1
+
+        full_name = data['full_name']
+        doctor.full_name = full_name
+
+        specialization = data['specialization']
+        doctor.specialization = specialization
+
+        hospital_name = data['hospital_name']
+        doctor.hospital_name = hospital_name
+
+        verified = data['verified']
+        doctor.verified = verified
+
+        document = data['document']
+        doctor.document = document
+
+        profile_picture = data['profile_picture']
+        doctor.profile_picture = profile_picture
+
+        doctor.save()
+
+        personal_info['full_name'] = full_name
+        personal_info['specialization'] = specialization
+        personal_info['hospital_name'] = hospital_name
+        personal_info['verified'] = verified
+        personal_info['document'] = document
+        personal_info['profile_picture'] = profile_picture
+
+    elif int(user.type) == UserType.MEMBER.value:
+        
+        try:
+            member = Member.objects.get(user=user)
+        except:
+            return Response({'error': 'User not found'}, status=400)
+
+        personal_info['type'] =  2
+        member_info = member.info
+
+        member_username = data['member_username']
+        member.member_username = member_username
+
+        firstname = data['firstname']
+        member_info.firstname = firstname
+
+        lastname = data['lastname']
+        member_info.lastname = lastname
+
+        address = data['address']
+        member_info.address = address
+
+        weight = data['weight']
+        member_info.weight = weight
+
+        height = data['height']
+        member_info.height = height
+
+        age = data['age']
+        member_info.age = age
+
+        avatar = data['avatar']
+        member_info.avatar = avatar
+
+        past_illnesses = data['past_illnesses']
+        member_info.past_illnesses = past_illnesses
+
+        allergies = data['allergies']
+        member_info.allergies = allergies
+
+        chronic_diseases = data['chronic_diseases']
+        member_info.chronic_diseases = chronic_diseases
+
+        undergone_operations = data['undergone_operations']
+        member_info.undergone_operations =undergone_operations
+
+        used_drugs = data['used_drugs']
+        member_info.used_drugs = used_drugs
+
+        member.save()
+        member_info.save()
+
+        personal_info['member_username'] = member_username
+        personal_info['firstname'] = firstname
+        personal_info['lastname'] = lastname
+        personal_info['address'] = address
+        personal_info['weight'] = weight
+        personal_info['height'] = height
+        personal_info['age'] = age
+        personal_info['avatar'] = avatar
+        personal_info['past_illnesses'] = past_illnesses
+        personal_info['allergies'] = allergies
+        personal_info['chronic_diseases'] = chronic_diseases
+        personal_info['undergone_operations'] = undergone_operations
+        personal_info['used_drugs'] = used_drugs
+
+
+    return Response(personal_info, status=200)
+
