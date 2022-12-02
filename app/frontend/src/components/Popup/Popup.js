273886cb10833b0533e33468d38bd09e2456bdb9
Yavuz Samet Topcuoglu <yavuuzsameet@Andres.local>
2022-12-02 21:08:40 +0300

    popup created for editing avatar

---
+++ app/frontend/src/components/Popup/Popup.js
@@ -0,0 +1,27 @@
+import { Button } from "antd";
+import React from "react";
+import './Popup.css'
+
+const editButton = {
+    width: "10%",
+    borderRadius: 50,
+    borderColor:'rgba(0,0,0,0.5)',
+    backgroundColor: 'rgb(255,255,255)',
+    color: 'rgb(104,172,252)',
+}
+
+function Popup(props) {
+    return (props.trigger) ? (
+        <div className="popup">
+            <div className="popup-inner">
+                <Button className="close-btn" style={editButton} onClick={() => props.setTrigger(false)}>
+                    Save
+                </Button>
+                {props.children}
+            </div>
+        </div>
+    ) : "";
+}
+
+export default Popup;
+
