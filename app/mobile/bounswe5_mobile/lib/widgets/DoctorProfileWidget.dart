7a108ffc096f27fbe9699d288d353679bc4b3ae5
enginoguzhansenol <engin.senol@boun.edu.tr>
2022-12-25 17:50:21 +0300

    Doctor profile page widget is created.

---
+++ app/mobile/bounswe5_mobile/lib/widgets/DoctorProfileWidget.dart
@@ -0,0 +1,64 @@
+import 'package:bounswe5_mobile/screens/editprofile.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:bounswe5_mobile/models/user.dart';
+import 'package:flutter_svg/svg.dart';
+
+class DoctorProfileWidget extends StatelessWidget{
+
+  final String profilePicture;
+  final String fullName;
+  final String specialization;
+  final String hospitalName;
+
+
+  const DoctorProfileWidget({
+    Key? key,
+    required this.profilePicture,
+    required this.fullName,
+    required this.specialization,
+    required this.hospitalName
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    final color = Theme.of(context).colorScheme.primary;
+    return Center(
+      child: Stack(
+        children: [
+          buildImage(context),
+        ],
+      ),
+    );
+  }
+  Widget buildImage(BuildContext context){
+    Widget pp;
+    String tempImagePath = 'lib/assets/images/generic_user.jpg';
+    if(profilePicture == null){
+      pp = Image.asset(tempImagePath);
+    }
+    else{
+      pp = Image.network(profilePicture!);
+    }
+    return CircleAvatar(
+        radius: 100.0,
+        child: ClipOval(
+          child: pp,
+        )
+    );
+  }
+
+  Widget buildCircle({
+    required Widget child,
+    required double all,
+    required Color color,
+  }) =>
+      ClipOval(
+        child: Container(
+          padding: EdgeInsets.all(all),
+          color: color,
+          child: child,
+        ),
+      );
+
+}
