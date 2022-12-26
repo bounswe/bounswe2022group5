d4be4f28d3c77f3b71ac894dac24953dafc0db37
enginoguzhansenol <engin.senol@boun.edu.tr>
2022-12-26 22:19:30 +0300

    Nullchecks are added for missing information.

--- app/mobile/bounswe5_mobile/lib/widgets/ProfileName.dart
+++ app/mobile/bounswe5_mobile/lib/widgets/ProfileName.dart
@@ -3,20 +3,27 @@ import 'package:flutter/material.dart';
 import 'package:bounswe5_mobile/models/user.dart';
 Widget buildName(User user) => Column(
     children: [
+      user.id == null?
+      SizedBox.shrink():
       Text(
         user.id.toString()
       ),
+      user.usertype == null?
+      SizedBox.shrink():
       Text(
           (user.usertype == 1) ?
           "Doctor":"Member"
       ),
       Text(
-          (user.usertype == 1) ? user.hospitalName: user.email,
+          (user.usertype == 1 && user.hospitalName != null)?
+          user.hospitalName:
+          (user.email != null? user.email: ""),
           style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18)
       ),
       Text(
-          (user.usertype == 1) ?
-          user.fullName:user.username,
+          (user.usertype == 1 && user.fullName != null)?
+          user.fullName:
+          (user.username != null? user.username: ""),
         style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
       ),
     ]
